67d46eb40d502545c5bc1dd3a0cd3ff7e4fd065f
Ffrybort <frybortfe@gmail.com>
2026-01-17 09:45:46 +0100

    moved transition into types.hh, added epsilon definition

--- include/mata/nfta/types.hh
+++ include/mata/nfta/types.hh
@@ -7,28 +7,56 @@
 #define NFTA_TYPES_HH
 
 #include "mata/alphabet.hh"
+#include <limits>
 
 namespace mata::nfta
 {
 
-/**
-* @brief A mapping of symbols to arities. Constants (arity 0) are not stored. Every symbol not stored is considered a constant.
-*
-*/
-struct ArityMap
-{
-public:
-    std::unordered_map<Symbol, unsigned> arities_; ///< Maps function symbols to arities; constants not stored
+    /**
+    * @brief A mapping of symbols to arities. Constants (arity 0) are not stored. Every symbol not stored is considered a constant.
+    *
+    */
+    struct ArityMap
+    {
+    public:
+        std::unordered_map<Symbol, unsigned> arities_; ///< Maps function symbols to arities; constants not stored
 
-    ArityMap() : arities_() {}
-    void set_arity(Symbol symbol, unsigned arity) { if (arity > 0) { arities_[symbol] = arity; } }
-    unsigned get_arity(Symbol symbol) const { return arities_.contains(symbol) ? arities_.at(symbol) : 0; }
-};
+        ArityMap() : arities_() {}
+        void set_arity(Symbol symbol, unsigned arity) { if (arity > 0) { arities_[symbol] = arity; } }
+        unsigned get_arity(Symbol symbol) const { return arities_.contains(symbol) ? arities_.at(symbol) : 0; }
+    };
 
-using State = unsigned;
-using StateVectorSet = utils::OrdVector<std::vector<State>>;
+    using State = unsigned;
+    using StateVectorSet = utils::OrdVector<std::vector<State>>;
+    constexpr Symbol EPSILON{ std::numeric_limits<Symbol>::max() };
 
+    /**
+     * @brief
+     */
+    struct Transition
+    {
+        Symbol symbol;
+        State source;
+        std::vector<State> targets;
 
-}
+        explicit Transition(
+            const Symbol symbol = {},
+            const State source = {},
+            const std::vector<State>& targets = {}
+        )
+            : symbol(symbol),
+              source(source),
+              targets(targets)
+        {
+        }
 
+        bool operator<(const Transition& other) const
+        {
+            if (source != other.source) return source < other.source;
+            if (symbol != other.symbol) return symbol < other.symbol;
+            if (targets.size() != other.targets.size()) return targets.size() < other.targets.size();
+            return targets < other.targets;
+        }
+    };
+} // namespace mata::nfta
 #endif //NFTA_TYPES_HH
