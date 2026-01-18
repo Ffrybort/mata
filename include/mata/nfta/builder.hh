d1995742cc06254662f99275a8e08da2478e313a
Ffrybort <frybortfe@gmail.com>
2026-01-18 20:44:53 +0100

    feat: parsing from mata format

--- include/mata/nfta/builder.hh
+++ include/mata/nfta/builder.hh
@@ -17,23 +17,23 @@ namespace mata::nfta {
      * @brief Parse top-down nfta from an intermediate automaton.
      *
      * @param inter_aut Intermediate automaton to parse.
-     * @param alphabet Compatible and already filled with symbols or OnTheFlyAlphabet.
+     * @param IntAlphabet, OnTheFlyAlphabet, or EnumAlphabet that is already filled with symbols.
      * @throws std::runtime_errro Alphabet is invalid or parsing fails.
      */
     Nfta construct_from_inter_aut(const IntermediateAut *inter_aut, Alphabet *alphabet);
 
     /**
-     *  @brief Parse from the mata nfta format in an input stream. IntAlphabet is used.
+     *  @brief Parse from the mata nfta format in an input stream.
      *
      * @throws std::runtime_error Parsing fails.
      */
-    Nfta parse_from_mata(std::istream& input);
+    Nfta parse_from_mata(std::istream& input, Alphabet *alphabet);
 
     /**
-     * @brief Parse from the mata nfta format in a string. IntAlphabet is used.
+     * @brief Parse from the mata nfta format in a string.
      *
      * @throws std::runtime_error Parsing fails.
      */
-    Nfta parse_from_mata(const std::string& input);
+    Nfta parse_from_mata(const std::string& input, Alphabet *alphabet);
 }
 #endif //NFTA_BUILDER_HH
